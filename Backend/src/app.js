14545f3e629bd9a5a7f0a1d8a24130f76dd27555
Art3mis20726 <gitpats@gmail.com>
2024-03-15 18:52:03 +0530

    main: Added

---
+++ Backend/src/app.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from"cors"
+import cookieParser from "cookie-parser"
+const app= express()
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+app.use(express.json({
+    limit:"1000kb"
+}))
+app.use(express.urlencoded({
+    extended:true,limit:"1000kb"
+}))
+app.use(express.static("public"))
+//This is for accesstoken and  refresh token to stay in req
+app.use(cookieParser())
+//For routing to the user routes
+import userRouter from "./routes/user.routes.js"
+import fileRouter from "./routes/file.routes.js"
+app.use("/api/v1/users",userRouter)//For user 
+app.use("/api/v1/files",fileRouter)//For uploading file 
+export default app
---
+++ Frontend/src/components/filesview/FilesView.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const FilesView = () => {
+  return (
+    <div className='fileView'>
+        <div className="fileView_row">
+
+        </div>
+        <div className="fileView_titles">
+            <div className="fileView_titles--left">
+                <p>Name</p>
+            </div>
+            <div className="fileView_titles--right">
+                <p>Last modified</p>
+                <p>File size</p>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
