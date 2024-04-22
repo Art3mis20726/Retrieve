df1089ab5fcfa53acfd4c45a146034437a4b10cc
Art3mis20726 <gitpats@gmail.com>
2024-04-22 15:54:30 +0530

    "Added FileList component to Home page"
    This commit message describes the code diff by stating that a new component, FileList, has been added to the Home route. It is concise and accurately reflects the changes made in the code.

--- Frontend/src/routes/Home/Home.js
+++ Frontend/src/routes/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import FileUpload from '../../components/fileupload/FileUpload'
+import FileList from '../../components/filelist/FileList'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
     <FileUpload/>
+    <FileList/>
     </>
   )
 }
